bc6200f8410b943b5a16278eff960d4715799681
Vogte999 <Vogt999@gmail.com>
2016-01-10 14:54:16 +0100

    minor bug fixes

--- src/quadratischeFunktionen/Main.java
+++ src/quadratischeFunktionen/Main.java
@@ -14,7 +14,7 @@ import javafx.stage.Stage;
 import javax.swing.*;
 import java.util.Scanner;
 
-public class Main extends Application {
+public class Main{
 
     Button button;
 
@@ -24,41 +24,6 @@ public class Main extends Application {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-
-        // launch(args);
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Geben Sie bitte die Parameter einer quadratischen Funktionen nach \"ax² + bx + c\" an.");
-
-        QuadraticFunction testFunction;
-
-        System.out.print("a: ");
-        double a = Double.parseDouble(scanner.next());
-        System.out.print("b: ");
-        double b = Double.parseDouble(scanner.next());
-        System.out.print("c: ");
-        double c = Double.parseDouble(scanner.next());
-
-        try {
-            testFunction = new QuadraticFunction(a, b, c);
-        } catch (IllegalArgumentException exception) {
-            System.out.println(exception.getMessage());
-            return;
-        }
-
-        System.out.println(testFunction.toNormalString());
-        System.out.println(testFunction.toVertexString());
-        System.out.println(testFunction.toZeroString());
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Quadratische Funktionen");
-        button = new Button("Klick mich!");
-
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
-        Scene scene = new Scene(layout, 300, 250);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
 }
