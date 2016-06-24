73d90d4cefc46f5a2c38b9ad5e426f2c800a5ead
Vogte <Vogt999@gmail.com>
2016-06-24 16:18:00 +0200

    Add calculator for a, b and c as input and add some styling by mmaterial design lite

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,8 +6,16 @@ class MainController < ApplicationController
     a = func_params[:a].to_f
     b = func_params[:b].to_f
     c = func_params[:c].to_f
-    func = QuadraticFunction.new(a, b, c)
-    @vertex = func.vertex
+    start = params[:start].to_f
+    step = params[:step].to_f
+    theend = params[:end].to_f
+    unless a == 0
+      func = QuadraticFunction.new(a, b, c)
+      @vertex = func.vertex
+      @zero1 = func.zero1
+      @zero2 = func.zero2
+      @images = func.getImagesOf2(start, theend, step) unless ((step == 0) || (step >= theend - start))
+    end
   end
 
   private def func_params
